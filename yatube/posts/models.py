fad9d4cf67e3a9301bed58038bd7a6828490c1f5
Александр Ершов <s9515411757@gamil.com>
2022-10-05 09:26:31 +0300

    текст коÐСпринт 3. 1 коммит

---
+++ yatube/posts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+
+class Group(models.Model):
+    text = models.CharField()
+    slug = models.CharField()
+    description = models.TextField()
+
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def __str__(self):
+        pass
---
+++ yatube/posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+app_name = 'posts'
+urlpatterns = [
+    # Главная страница
+    path('', views.index, name='posts_string'),
+    path('group/<slug:slug>/', views.group_posts),
+]
