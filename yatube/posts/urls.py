621aa4f90e1a6169d04ce4c83c3028107250edb8
Александр Ершов <s9515411757@gamil.com>
2022-10-08 18:49:32 +0300

    Спринт 3. Финал 2.

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    # Главная страница
     path('', views.index, name='posts_string'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -106,8 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
